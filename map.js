ee046df8bff2a63689abda90f604c85de48fd04a
Avner Shanan <avner.shanan@devbootcamp.com>
2018-03-04 09:37:27 -0600

    Masking PoC

--- map.js
+++ map.js
@@ -1,12 +1,31 @@
 function draw(){
-    var canvas = document.getElementById('map');
-    var context = canvas.getContext('2d');
+    var mapCanvas,
+        mapContext,
+        maskCanvas,
+        maskContext,
+        mapImg;
 
-    context.fillRect(0, 0, 350, 150);
+    // create mask
+    maskCanvas = document.getElementById('mask');
+    maskContext = maskCanvas.getContext('2d');
 
-    context.fillStyle = 'rgba(225,225,225,1)';
-    context.fillRect(50,50,75,50);
-    context.globalAlpha = 1.0;
+    // black out canvas
+    maskContext.fillStyle = '#f2e4b3';
+    maskContext.fillRect(0, 0, 350, 150);
+    // add a transparent section
+    // maskContext.fillStyle = 'rgba(225,225,225,1)';
+    // maskContext.fillRect(50,50,75,50);
+    maskContext.clearRect(50,50,75,50)
+
+    // create background map
+    mapCanvas = document.getElementById('map');
+    mapContext = mapCanvas.getContext('2d');
+
+    mapImg = document.getElementById('img');
+    mapContext.drawImage(mapImg, 0, 0);
+
+    // lay mask over
+    mapContext.drawImage(maskCanvas, 0, 0);    
 }
 
 window.onload = draw;
